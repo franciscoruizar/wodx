a7e56b9fd085faa46f57ad7750fa387922b085dc
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2021-01-16 12:14:33 -0300

    Update dumps

--- src/accounts/main/resources/database/accounts.sql
+++ src/accounts/main/resources/database/accounts.sql
@@ -1,67 +1,93 @@
-CREATE TABLE IF NOT EXISTS `accounts`.`permissions` (
-  `id` CHAR(36) NOT NULL,
-  `description` VARCHAR(45) NOT NULL,
-  `entity` VARCHAR(45) NOT NULL,
-  `create` TINYINT NOT NULL,
-  `delete` TINYINT NOT NULL,
-  `update` TINYINT NOT NULL,
-  `read` TINYINT NOT NULL,
-  `read_all` TINYINT NOT NULL,
-  PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+--
+-- Table structure for table `companies`
+--
 
-CREATE TABLE IF NOT EXISTS `accounts`.`roles` (
-  `id` CHAR(36) NOT NULL,
-  `description` VARCHAR(45) NULL,
+DROP TABLE IF EXISTS `companies`;
+CREATE TABLE `companies` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `media_url` text COLLATE utf8mb4_unicode_ci,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `accounts`.`role_permissions` (
-  `id` CHAR(36) NOT NULL,
-  `permission_id` CHAR(36) NOT NULL,
-  `role_id` CHAR(36) NOT NULL,
+--
+-- Table structure for table `permissions`
+--
+
+DROP TABLE IF EXISTS `permissions`;
+CREATE TABLE `permissions` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `entity` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `create` tinyint NOT NULL,
+  `delete` tinyint NOT NULL,
+  `update` tinyint NOT NULL,
+  `read` tinyint NOT NULL,
+  `read_all` tinyint NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `role_permissions`
+--
+
+DROP TABLE IF EXISTS `role_permissions`;
+CREATE TABLE `role_permissions` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `permission_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_role_permissions_permissions` (`permission_id` ASC) VISIBLE,
-  INDEX `fk_role_permissions_roles` (`role_id` ASC) VISIBLE,
-  CONSTRAINT `fk_role_permissions_permissions`
-    FOREIGN KEY (`permission_id`)
-    REFERENCES `accounts`.`permissions` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_role_permissions_roles`
-    FOREIGN KEY (`role_id`)
-    REFERENCES `accounts`.`roles` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `fk_role_permissions_permissions` (`permission_id`),
+  KEY `fk_role_permissions_roles` (`role_id`),
+  CONSTRAINT `fk_role_permissions_permissions` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`),
+  CONSTRAINT `fk_role_permissions_roles` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `accounts`.`companies` (
-  `id` CHAR(36) NOT NULL,
-  `description` VARCHAR(45) NULL,
-  `media_url` TEXT NULL,
-  `latitude` DOUBLE NULL,
-  `longitude` DOUBLE NULL,
-  `is_active` BOOLEAN NULL,
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `accounts`.`users` (
-  `id` CHAR(36) NOT NULL,
-  `email` VARCHAR(320) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `company_id` CHAR(36) NOT NULL,
-  `role_id` CHAR(36) NOT NULL,
+--
+-- Table structure for table `user_companies`
+--
+
+DROP TABLE IF EXISTS `user_companies`;
+CREATE TABLE `user_companies` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `company_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_user_company` (`company_id` ASC) VISIBLE,
-  INDEX `fk_users_roles` (`role_id` ASC) VISIBLE,
-  CONSTRAINT `fk_user_company`
-    FOREIGN KEY (`company_id`)
-    REFERENCES `accounts`.`companies` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_users_roles`
-    FOREIGN KEY (`role_id`)
-    REFERENCES `accounts`.`roles` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `fk_user_companies_company` (`company_id`),
+  KEY `fk_user_companies_user` (`user_id`),
+  CONSTRAINT `fk_user_companies_company` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`),
+  CONSTRAINT `fk_user_companies_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(320) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `surname` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(17) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Dump completed on 2021-01-16 12:03:57
--- src/workouts/main/resources/database/workouts.sql
+++ src/workouts/main/resources/database/workouts.sql
@@ -1,61 +1,86 @@
-CREATE TABLE IF NOT EXISTS `workouts`.`step_types` (
-  `id` CHAR(36) NOT NULL,
-  `description` VARCHAR(45) NOT NULL,
-  `language` CHAR(20) NOT NULL,
+--
+-- Table structure for table `exercises`
+--
+
+DROP TABLE IF EXISTS `exercises`;
+CREATE TABLE `exercises` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `media_url` text COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `workouts`.`workouts` (
-  `id` CHAR(36) NOT NULL,
-  `start_date` DATETIME NOT NULL,
-  `end_date` DATETIME NOT NULL,
-  `coach_id` CHAR(36) NULL,
+--
+-- Table structure for table `step_types`
+--
+
+DROP TABLE IF EXISTS `step_types`;
+CREATE TABLE `step_types` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `language` char(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `workouts`.`steps` (
-  `id` CHAR(36) NOT NULL,
-  `description` VARCHAR(45) NULL,
-  `step_type_id` CHAR(36) NOT NULL,
-  `workout_id` CHAR(36) NOT NULL,
-  PRIMARY KEY (`id`, `step_type_id`, `workout_id`),
-  INDEX `fk_steps_step_types` (`step_type_id` ASC) VISIBLE,
-  INDEX `fk_steps_workouts` (`workout_id` ASC) VISIBLE,
-  CONSTRAINT `fk_steps_step_types`
-    FOREIGN KEY (`step_type_id`)
-    REFERENCES `workouts`.`step_types` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_steps_workouts`
-    FOREIGN KEY (`workout_id`)
-    REFERENCES `workouts`.`workouts` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+--
+-- Table structure for table `steps`
+--
 
-CREATE TABLE IF NOT EXISTS `workouts`.`exercises` (
-  `id` CHAR(36) NOT NULL,
-  `description` VARCHAR(45) NULL,
-  `media_url` VARCHAR(45) NULL,
-  `step_id` CHAR(36) NOT NULL,
+DROP TABLE IF EXISTS `steps`;
+CREATE TABLE `steps` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `step_type_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `workout_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`,`step_type_id`,`workout_id`),
+  KEY `fk_steps_step_types` (`step_type_id`),
+  KEY `fk_steps_workouts` (`workout_id`),
+  CONSTRAINT `fk_steps_step_types` FOREIGN KEY (`step_type_id`) REFERENCES `step_types` (`id`),
+  CONSTRAINT `fk_steps_workouts` FOREIGN KEY (`workout_id`) REFERENCES `workouts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `workout_exercises`
+--
+
+DROP TABLE IF EXISTS `workout_exercises`;
+CREATE TABLE `workout_exercises` (
+  `id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `series` int NOT NULL,
+  `value` int NOT NULL,
+  `time_unit` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `exercise_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `workout_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_exercises_steps` (`step_id` ASC) VISIBLE,
-  CONSTRAINT `fk_exercises_steps`
-    FOREIGN KEY (`step_id`)
-    REFERENCES `workouts`.`steps` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `fk_workout_exercise_exercises` (`exercise_id`),
+  KEY `fk_workout_exercise_workouts` (`workout_id`),
+  CONSTRAINT `fk_workout_exercise_exercises` FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`id`),
+  CONSTRAINT `fk_workout_exercise_workouts` FOREIGN KEY (`workout_id`) REFERENCES `workouts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `workouts`.`workout_users` (
-  `id` INT NOT NULL,
-  `workout_id` CHAR(36) NOT NULL,
-  `user_id` CHAR(36) NOT NULL,
+--
+-- Table structure for table `workout_users`
+--
+
+DROP TABLE IF EXISTS `workout_users`;
+CREATE TABLE `workout_users` (
+  `id` int NOT NULL,
+  `workout_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_workout_users_workouts1_id` (`workout_id` ASC) VISIBLE,
-  CONSTRAINT `fk_workout_users_workouts`
-    FOREIGN KEY (`workout_id`)
-    REFERENCES `workouts`.`workouts` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `fk_workout_users_workouts1_id` (`workout_id`),
+  CONSTRAINT `fk_workout_users_workouts` FOREIGN KEY (`workout_id`) REFERENCES `workouts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `workouts`
+--
+
+DROP TABLE IF EXISTS `workouts`;
+CREATE TABLE `workouts` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `coach_id` char(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
