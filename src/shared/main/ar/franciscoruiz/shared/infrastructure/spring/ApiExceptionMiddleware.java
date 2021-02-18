d3a8fdd8e198aa3085cdc5d26b6a2183df089184
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2021-02-18 15:36:04 -0300

    Add ExpiredJwtException in ApiExceptionMiddleware

--- src/shared/main/ar/franciscoruiz/shared/infrastructure/spring/ApiExceptionMiddleware.java
+++ src/shared/main/ar/franciscoruiz/shared/infrastructure/spring/ApiExceptionMiddleware.java
@@ -1,6 +1,7 @@
 package ar.franciscoruiz.shared.infrastructure.spring;
 
 import ar.franciscoruiz.shared.domain.DomainError;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import java.util.Date;
 
 @ControllerAdvice
 public class ApiExceptionMiddleware extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request) {
 
@@ -30,8 +32,25 @@ public class ApiExceptionMiddleware extends ResponseEntityExceptionHandler {
             );
     }
 
+    @ExceptionHandler(value = {ExpiredJwtException.class})
+    public ResponseEntity<Object> handleAnyException(ExpiredJwtException ex, WebRequest request) {
+
+        String errorMessageDescription = ex.getLocalizedMessage();
+
+        if (errorMessageDescription == null) errorMessageDescription = ex.toString();
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
+
+        return new ResponseEntity<>
+            (
+                errorMessage,
+                new HttpHeaders(),
+                HttpStatus.INTERNAL_SERVER_ERROR
+            );
+    }
+
     @ExceptionHandler(value = {NullPointerException.class})
-    public ResponseEntity<Object> handleSpecificExceptions(Exception ex, WebRequest request) {
+    public ResponseEntity<Object> handleSpecificExceptions(NullPointerException ex, WebRequest request) {
 
         String errorMessageDescription = ex.getLocalizedMessage();
 
@@ -45,7 +64,7 @@ public class ApiExceptionMiddleware extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(value = {DomainError.class, RuntimeException.class})
-    public ResponseEntity<Object> handleNotExistsExceptions(Exception ex, WebRequest request) {
+    public ResponseEntity<Object> handleNotExistsExceptions(DomainError ex, WebRequest request) {
 
         String errorMessageDescription = ex.getLocalizedMessage();
 
